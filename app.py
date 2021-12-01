a67f3792c02db44dc4bd9e8f13a0602c60467805
ash-02 <ashwins.2019@vitstudent.ac.in>
2021-12-02 04:57:47 +0530

    final commit

--- app.py
+++ app.py
@@ -12,6 +12,7 @@
 #     app.run()
 
 from itertools import product
+from os import name
 import re
 from dns.message import Message
 from flask import Flask,jsonify,request
@@ -19,7 +20,7 @@ from flask import render_template
 from flask.json import jsonify
 from nltk.util import pr
 # from pymongo import message
-from flask import flask_pymongo as fp
+# from flask import flask_pymongo as fp
 # from fp import PyMongo
 import gridfs
 import process1
@@ -30,8 +31,8 @@ app = Flask(__name__)
 client = pymongo.MongoClient("mongodb+srv://root:root@cluster0.aq5jb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
 db = client.get_database('mongodata')
 # mongo = pymongo.PyMongo(app)
-mongo = fp.PyMongo(app)
-fs = gridfs.GridFS(mongo.db)
+# mongo = fp.PyMongo(app)
+# fs = gridfs.GridFS(mongo.db)
 
 # @app.route('/',methods = ['GET', 'POST'])
 # def home():  # put application's code here
@@ -39,7 +40,7 @@ fs = gridfs.GridFS(mongo.db)
 
 @app.route('/',methods = ['GET', 'POST'])
 def index():  # put application's code here
-    return render_template("login.html")
+    return render_template("login1.html")
 
 @app.route('/login',methods=['GET','POST'])
 def login():
@@ -52,9 +53,9 @@ def login():
         if user_found['password']==password:
             return render_template('home.html',user = username.capitalize())
         else:
-            return render_template('login.html',message="wrong password")
+            return render_template('login1.html',message="wrong password")
     else:
-        return render_template('login.html',message="user not found")
+        return render_template('login1.html',message="user not found")
 
 @app.route('/register',methods=['GET','POST'])
 def register():
@@ -65,15 +66,15 @@ def register():
     password = request.form['pass']
 
     if records.find_one({"name":username}):
-        return render_template('login.html',message="username already exists")
+        return render_template('login1.html',message="username already exists")
     elif records.find_one({"email":email}):
-        return render_template('login.html',message="email already exists")
+        return render_template('login1.html',message="email already exists")
     elif records.find_one({"phone":phone}):
-        return render_template('login.html',message="email already exists")
+        return render_template('login1.html',message="email already exists")
     else:
         user_input = {'username': username, 'email': email, 'phone':phone,'password': password}
         records.insert_one(user_input)
-        return render_template('login.html',message="user successfully registered. Please login")
+        return render_template('login1.html',message="user successfully registered. Please login")
 
 @app.route('/speech_to_image',methods = ['POST','GET'])
 def speech_to_image():
@@ -96,7 +97,7 @@ def word_by_word():
     #     records.insert_one(user_input)
     #####
     # print(everything[1][1])
-    return render_template('display.html',everything=everything,len=len(everything),flag=0)
+    return render_template('display.html',everything=everything,len=len(everything),flag=0,text=text)
 
 @app.route('/whole_word',methods = ['POST','GET'])
 def whole_word():
@@ -118,10 +119,23 @@ def whole_word():
     everything_word=[]
     for word in everything:
         everything_word.append(tuple((text,word[1],word[2])))
-    return render_template('display.html',everything=everything_word,len=len(everything),flag=1)
+    return render_template('display.html',everything=everything_word,len=len(everything),flag=1,text=text)
 
 @app.route('/addprodpage',methods = ['POST','GET'])
 def addprodpage():
+    return render_template('/admin/addprod.html')
+
+@app.route('/addprod',methods = ['POST','GET'])
+def addprod():
+    records=db.products
+    prodname=request.form['pname']
+    compname=request.form['cname']
+    price=request.form['price']
+    desc=request.form['description']
+    pimage=request.form['prodimage']
+
+    prod_input={'pname':prodname,'cname':compname,'price':price,'description':desc,'image':pimage}
+    records.insert_one(prod_input)
     return render_template('/admin/addprod.html')
 
 # @app.route('/addprod',methods = ['POST','GET'])
@@ -131,43 +145,201 @@ def addprodpage():
 #     compname=request.form['cname']
 #     price=request.form['price']
 #     desc=request.form['description']
-#     prodimage=request.form['prodimage']
+#     # prodimage = request.form['prodimage']
+#     # if 'prodimage' in request.files:
+#     #     prodimageName=request.files['prodimage']
+#     #     fss = gridfs.GridFS(db)
+#     #     fss.put(prodimageName,filename=prodimageName.filename)
+#     #     # return render_template('admin.html')
 
 #     prod_input={'pname':prodname,'cname':compname,'price':price,'description':desc,'image':prodimage}
 #     records.insert_one(prod_input)
 #     return render_template('/admin/addprod.html')
 
-@app.route('/addprod',methods = ['POST','GET'])
-def addprod():
-    records=db.products
-    prodname=request.form['pname']
-    compname=request.form['cname']
-    price=request.form['price']
-    desc=request.form['description']
-    prodimage = request.form['prodimage']
-    if 'prodimage' in request.files:
-        prodimageName=request.files['prodimage']
-        fss = gridfs.GridFS(db)
-        fss.put(prodimageName,filename=prodimageName.filename)
-        # return render_template('admin.html')
-
-    # prod_input={'pname':prodname,'cname':compname,'price':price,'description':desc,'image':prodimage}
-    # records.insert_one(prod_input)
-    return render_template('/admin/addprod.html')
-@app.route('/file/<filename>')
-def file(filename):
-    return mongo.send_file(filename)
+# @app.route('/file/<filename>')
+# def file(filename):
+#     return mongo.send_file(filename)
 
 @app.route('/prodshow',methods = ['POST','GET'])
 def prodshow():
     records=db.products
     products=records.find()
+    products1=records.find()
+
+    records2=db.cart
+    val = records2.count_documents({})
 
     data = db.fs.files.find()
     # my_id = data['_id']
     # outputdata = fs.get(my_id).read()
+    minprod=products.sort("price", 1).limit(1);
+    maxprod=products1.sort("price", -1).limit(1);
+    for minp in minprod:
+        min=minp['price']
+    for maxp in maxprod:
+        max=maxp['price']
+    print(min)
+    return render_template('products.html',products=records.find(),min=min,max=max,filter=records.find(),val=val)
 
-    return render_template('products.html',products=records.find(),min=products.sort("price", -1).limit(1),max=products.sort("price", 1).limit(1),img=data)
+@app.route('/to_admin',methods = ['POST','GET'])
+def to_admin():
+    return render_template('/admin/admin.html')
+
+@app.route('/inventory_check',methods = ['POST','GET'])
+def inventory_check():
+    records=db.products
+    return render_template('/admin/inventory.html',products=records.find())
+
+@app.route('/users_check',methods = ['POST','GET'])
+def users_check():
+    records=db.user
+    users=records.find()
+    # for user in users:
+    #     print(user['username'])
+    return render_template('/admin/adminusers.html',users=users)
+
+
+@app.route('/delete_prod',methods = ['POST','GET'])
+def delete_prod():
+    records=db.products
+    prodname=request.form['delete_id']
+    myQuery ={'pname':prodname}
+    records.delete_one(myQuery)
+    return render_template('/admin/inventory.html',products=records.find())
+
+@app.route('/to_edit_prod',methods = ['POST','GET'])
+def to_edit_prod():
+    edit_id=request.form['edit_id']
+    records=db.products
+    prod=records.find_one({"pname":edit_id})
+    # for prod in products:
+    #     print(type(prod))
+    # print(type(products))
+    return render_template('/admin/editprod.html',edit=edit_id,prod=prod)
+
+@app.route('/edit_prod',methods = ['POST','GET'])
+def edit_prod():
+    records=db.products
+    prodname_edit=request.form['edit_id']
+    prodname=request.form['pname']
+    compname=request.form['cname']
+    price=request.form['price']
+    desc=request.form['description']
+
+    records.find_one_and_update({'pname':prodname_edit},
+                                {'$set':{'pname':prodname,'cname':compname,'price':price,'description':desc}})
+
+    return render_template('/admin/inventory.html',products=records.find())
+
+@app.route('/admin',methods=['GET','POST'])
+def admin():
+    records = db.admin
+    username=request.form['auname']
+    password=request.form['apass']
+    
+    user_found = records.find_one({"username": username})
+    if user_found:
+        if user_found['password']==password:
+            return render_template('/admin/admin.html')
+        else:
+            return render_template('login1.html',message="wrong admin credentials")
+    else:
+        return render_template('login1.html',message="wrong admin credentials")
+
+@app.route('/pricefilter',methods = ['POST','GET'])
+def pricefilter():
+    records=db.products
+    products=records.find()
+    products1=records.find()
+
+    mina=request.form['min']
+    maxa=request.form['max']
+
+    query = records.find({'price': { '$gt': mina, '$lt': maxa}})
+
+    minprod=products.sort("price", 1).limit(1);
+    maxprod=products1.sort("price", -1).limit(1);
+    for minp in minprod:
+        min=minp['price']
+    for maxp in maxprod:
+        max=maxp['price']
+
+    return render_template('products.html',products=query,min=min,max=max,filter=records.find())
+
+    # return render_template('products.html')
+
+@app.route('/brand',methods = ['POST','GET'])
+def brand():
+    print("in")
+    records=db.products
+    products=records.find()
+    products1=records.find()
+
+    com=request.form['brand']
+
+    query = records.find({'cname': com})
+
+    minprod=products.sort("price", 1).limit(1);
+    maxprod=products1.sort("price", -1).limit(1);
+    for minp in minprod:
+        min=minp['price']
+    for maxp in maxprod:
+        max=maxp['price']
+
+    return render_template('products.html',products=query,min=min,max=max,filter=records.find())
+
+@app.route('/addcart',methods = ['POST','GET'])
+def addcart():
+    records1=db.cart
+    prodname=request.form['cpname']
+    compname=request.form['ccname']
+    price=request.form['cprice']
+    desc=request.form['cdescription']
+    pimage=request.form['cimage']
+
+    prod_input={'pname':prodname,'cname':compname,'price':price,'description':desc,'image':pimage}
+    records1.insert_one(prod_input)
+
+    records=db.products
+    products=records.find()
+    products1=records.find()
+
+    records2=db.cart
+    val = records2.count_documents({})
+
+    data = db.fs.files.find()
+    # my_id = data['_id']
+    # outputdata = fs.get(my_id).read()
+    minprod=products.sort("price", 1).limit(1);
+    maxprod=products1.sort("price", -1).limit(1);
+    for minp in minprod:
+        min=minp['price']
+    for maxp in maxprod:
+        max=maxp['price']
+
+    return render_template('products.html',products=records.find(),min=min,max=max,filter=records.find(),val=val)
+
+@app.route('/to_cart',methods = ['POST','GET'])
+def to_cart():
+    records=db.cart
+    totalcost=0
+    cartprods = records.find()
+    for cprod in cartprods:
+        totalcost+=int(cprod['price'])
+    return render_template('cart.html',products=records.find(),totalcost=totalcost)
+
+@app.route('/delete_cart',methods = ['POST','GET'])
+def delete_cart():
+    records=db.cart
+    prodname=request.form['delete_id']
+    myQuery ={'pname':prodname}
+    records.delete_one(myQuery)
+    totalcost=0
+    cartprods = records.find()
+    for cprod in cartprods:
+        totalcost+=int(cprod['price'])
+    return render_template('cart.html',products=records.find(),totalcost=totalcost)
+
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=False)
